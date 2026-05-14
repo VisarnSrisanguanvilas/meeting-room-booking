363525db449400adca679867a3062cad3682ecb5
visarn <visarn.sri@gmail.com>
2026-05-14 16:48:21 +0700

    fix: main.py

--- backend/main.py
+++ backend/main.py
@@ -27,3 +27,8 @@ app.add_middleware(
 @app.get("/health")
 def health_check():
     return{"ping":"pong"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
